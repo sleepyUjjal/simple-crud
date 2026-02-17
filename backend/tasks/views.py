7865035b81e591e77c415d93175de975501aacb7
Ujjaldeep Singh <152362390+Ujjaldeep@users.noreply.github.com>
2026-02-17 17:04:01 +0530

    Made Migrations

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -4,6 +4,7 @@ from .serializers import TaskSerializer
 from .permissions import IsOwnerOrAdmin
 
 class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
 
