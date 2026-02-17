c41821a8ccf69a458cccef6dac847df9f607ad1b
Ujjaldeep Singh <152362390+Ujjaldeep@users.noreply.github.com>
2026-02-17 16:49:53 +0530

    Setup accounts app

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('USER', 'User'),
+        ('ADMIN', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='USER')
+
+    def __str__(self):
+        return self.username
---
+++ backend/accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'role')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data.get('role', 'USER')
+        )
+        return user
